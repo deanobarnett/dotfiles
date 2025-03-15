706a8093a0b032ba9771a0cc86752f5fd18c14b6
deano <mail@deano.io>
2025-03-15 20:21:04 +0000

    clean

--- config/.config/fish/functions/yy.fish
+++
@@ -1,8 +0,0 @@
-function yy
-    set tmp (mktemp -t "yazi-cwd.XXXXXX")
-    yazi $argv --cwd-file=$tmp
-    if set cwd (cat $tmp); and test -n "$cwd" -a "$cwd" != "$PWD"
-        cd $cwd
-    end
-    rm -f $tmp
-end
